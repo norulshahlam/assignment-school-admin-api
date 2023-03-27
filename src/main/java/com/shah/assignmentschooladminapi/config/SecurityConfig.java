25b4b0b57dea7eb842ed5dbdf333ee9b338015c1
norulshahlam <norulshahlam@gmail.com>
2023-03-27 10:49:41 +0800

    hashed password

--- src/main/java/com/shah/assignmentschooladminapi/config/SecurityConfig.java
+++ src/main/java/com/shah/assignmentschooladminapi/config/SecurityConfig.java
@@ -12,9 +12,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-/**
- * @author NORUL
- */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -22,21 +19,30 @@ public class SecurityConfig {
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
 
+        // Using Spring to encode password for you - Not secure
+        UserDetails user = User
+                .withUsername("user")
+                .password((passwordEncoder().encode("password")))
+                .roles("USER")
+                .build();
+
+        // You encode yourself then insert - secure
         UserDetails admin = User
                 .withUsername("admin")
-                .password((passwordEncoder().encode("password")))
+                .password("$2a$09$9j2c1BTj4zMU.oaaSNumhOfgYuK21hfNiDR.H8HoE677Vh3kPsuQC")
                 .roles("ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(admin);
+
+        return new InMemoryUserDetailsManager(admin,user);
     }
 
     /**
      * Allow endpoint with specific roles
      * Allow h2 console
-     * <a href="https://www.youtube.com/watch?v=awcCiqBO36E&list=PLZV0a2jwt22s5NCKOwSmHVagoDW8nflaC&index=7">...</a>
+     * https://www.youtube.com/watch?v=awcCiqBO36E&list=PLZV0a2jwt22s5NCKOwSmHVagoDW8nflaC&index=7
      *
-     * @param http Http call
-     * @return SecurityFilterChain
+     * @param http
+     * @return
      * @throws Exception
      */
     @Bean
